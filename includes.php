80437253738be67346e6a51525a477742bfa164f
Anderson Grüdtner Martins <andergmartins@gmail.com>
2023-05-05 10:31:15 -0300

    Update includes.php

--- includes.php
+++ includes.php
@@ -1,5 +1,5 @@
 <?php
 
-if (! defined('PUBLISHPRESS_SERIES_VENDOR_PATH')) {
-    define('PUBLISHPRESS_SERIES_VENDOR_PATH', realpath(__DIR__ . '/../../'));
+if (! defined('PUBLISHPRESS_SERIES_PRO_VENDOR_PATH')) {
+    define('PUBLISHPRESS_SERIES_PRO_VENDOR_PATH', realpath(__DIR__ . '/../../'));
 }
